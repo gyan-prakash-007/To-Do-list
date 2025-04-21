2779dbe4d4d59ee4501175a9eb431cc063100721
Gyan Prakash <gyansir22@gmail.com>
2025-04-21 17:30:35 +0530

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const listContainer  = document.getElementById("list-container");
 
 function addTask(){
     if(inputBox.value == ''){
-        alert("you must writee something");
+        alert("you must write something");
     }
     else{
         let li = document.createElement("li");
